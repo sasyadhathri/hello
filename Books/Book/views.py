ab7fce98140259cfc61448a05f5a045bac2b8455
sasyadhathri <sasyadhathri@gmail.com>
2022-08-29 11:36:20 +0530

    first commit

---
+++ Books/Book/views.py
@@ -0,0 +1,37 @@
+from pickle import GET
+from django.shortcuts import render
+from .serializers import *
+from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+#read
+@api_view(['GET'])
+def Booklist(request):
+    booksobj=BooksModel.objects.all()#queryset
+    serializer=BookSerializer(booksobj,many=True)
+    return Response(serializer.data)
+#create
+@api_view(['POST'])
+def post_Book(request):
+    booksobj=BooksModel.objects.all()#queryset
+    serializer=BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+#update
+@api_view(['POST'])
+def update_Book(request,id):
+    booksobj=BooksModel.objects.get(id=id)#queryset
+    serializer=BookSerializer(instance=booksobj,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+#delete
+@api_view(['DELETE'])
+def delete_Book(request,id):
+    booksobj=BooksModel.objects.get(id=id)#queryset
+    booksobj.delete()
+    return Response("book is heavy")
